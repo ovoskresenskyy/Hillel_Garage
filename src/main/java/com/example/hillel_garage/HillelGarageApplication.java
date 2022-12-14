06696e1a0d5a2b5fa1fc34ae31182c33a286063e
ovoskresenskiy <7949894@ukr.net>
2022-12-14 23:47:42 +0200

    Init commit

---
+++ src/main/java/com/example/hillel_garage/HillelGarageApplication.java
@@ -0,0 +1,13 @@
+package com.example.hillel_garage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HillelGarageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HillelGarageApplication.class, args);
+    }
+
+}
