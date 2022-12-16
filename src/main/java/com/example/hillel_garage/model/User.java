f74cf0e9bf881882da1996ba83314d74c81a83d6
ovoskresenskiy <7949894@ukr.net>
2022-12-16 21:54:16 +0200

    Not need to collect cars here, moved to Car Service

--- src/main/java/com/example/hillel_garage/model/User.java
+++ src/main/java/com/example/hillel_garage/model/User.java
@@ -3,13 +3,10 @@ package com.example.hillel_garage.model;
 import lombok.Builder;
 import lombok.Data;
 
-import java.util.List;
-
 @Data
 @Builder
 public class User {
     private String name;
     private int age;
     private int id;
-    private List<Car> cars;
 }
