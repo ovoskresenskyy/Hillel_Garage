0a40fab94f35080e62539593000d535280eb7758
ovoskresenskyy <7949894@ukr.net>
2022-12-17 20:40:59 +0200

    Minor code cleaning

--- src/main/java/com/example/hillel_garage/controller/CarController.java
+++ src/main/java/com/example/hillel_garage/controller/CarController.java
@@ -1,15 +1,11 @@
 package com.example.hillel_garage.controller;
 
 import com.example.hillel_garage.model.Car;
-import com.example.hillel_garage.model.User;
 import com.example.hillel_garage.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/cars")
