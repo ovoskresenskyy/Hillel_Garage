6df30ac5ae2d01e51fc6ca7962142408f0a3beea
ovoskresenskyy <7949894@ukr.net>
2022-12-20 15:31:11 +0200

    Totally rebuild controller

--- src/main/java/com/example/hillel_garage/controller/CarController.java
+++ src/main/java/com/example/hillel_garage/controller/CarController.java
@@ -3,11 +3,11 @@ package com.example.hillel_garage.controller;
 import com.example.hillel_garage.model.Car;
 import com.example.hillel_garage.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-@RestController
+@Controller
 @RequestMapping("/cars")
 public class CarController {
 
@@ -18,29 +18,40 @@ public class CarController {
         this.carService = carService;
     }
 
-    @PostMapping
-    public Car addCar(@RequestBody Car car){
-        return carService.addCar(car);
-    }
-
-    @GetMapping("/{id}")
-    public Car getCar(@PathVariable int id) {
-        return carService.getCar(id);
-    }
-
     @GetMapping
-    public List<Car> getAll(){
-        return carService.getAll();
+    public String getAllCars(Model model) {
+        model.addAttribute("cars", carService.getAll());
+        return "cars/list";
     }
 
-    @PutMapping("/{id}")
-    public Car updateColor(@PathVariable int id, @RequestParam(name = "color") String color) {
-        return carService.updateColor(id, color);
+    @GetMapping("/add_new")
+    public String addNewCar(Model model) {
+        model.addAttribute("car", Car.builder().build());
+        return "/cars/newcar";
     }
 
-    @DeleteMapping("/{id}")
-    public Car deleteUser(@PathVariable int id) {
-        return carService.deleteCar(id);
+    @PostMapping("/save")
+    public String saveCar(@ModelAttribute("car") Car car) {
+        carService.addCar(car);
+        return "redirect:/cars";
     }
 
+    @GetMapping("/update_form/{id}")
+    public String updateForm(@PathVariable(value = "id") int id,
+                             Model model) {
+        model.addAttribute("car", carService.getCar(id));
+        return "cars/updatecar";
+    }
+
+    @PostMapping("/update")
+    public String updateCar(@ModelAttribute("car") Car car) {
+        carService.updateCar(car);
+        return "redirect:/cars";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteCar(@PathVariable(value = "id") int id) {
+        carService.deleteCar(id);
+        return "redirect:/cars";
+    }
 }
